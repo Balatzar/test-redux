af72516df916c341b267d744b5384c8b8d5328eb
balatzar <bataprod@gmail.com>
2016-03-10 17:45:16 +0100

    high scores

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ var gameStarted = false;
 var start;
 
 var SPEED = localStorage.getItem("speed") ? localStorage.getItem("speed") : 600;
+var DIFFICULTY = localStorage.getItem("difficulty") ? localStorage.getItem("difficulty") : "Moyen";
 
 speedNode.innerHTML = SPEED;
 
@@ -26,6 +27,7 @@ var whack = function(state, action) {
       points: 0,
       speed: localStorage.getItem("speed"),
       start: false,
+      difficulty: "facile",
     };
   }
   switch (action.type) {
@@ -36,6 +38,7 @@ var whack = function(state, action) {
           points: state.points,
           speed: state.speed,
           start: state.start,
+          difficulty: state.difficulty,
       };
     case "CLICK_HOLE":
       return {
@@ -44,6 +47,7 @@ var whack = function(state, action) {
           points: state.points + 1,
           speed: state.speed,
           start: state.start,
+          difficulty: state.difficulty,
       };
     case "SET_SPEED":
       return {
@@ -52,6 +56,7 @@ var whack = function(state, action) {
           points: state.points,
           speed: action.speed,
           start: state.start,
+          difficulty: action.difficulty,
       };
     case "START_GAME":
       return {
@@ -60,6 +65,7 @@ var whack = function(state, action) {
           points: state.points,
           speed: state.speed,
           start: true,
+          difficulty: state.difficulty,
       };
     default :
       return state;
@@ -72,6 +78,22 @@ var win = function() {
   var seconds = Math.floor(num / 1000);
   var minutes = Math.floor(seconds / 60);
   seconds = seconds - (minutes * 60);
+  var score = {
+    seconds: seconds,
+    minutes: minutes,
+    difficulty: DIFFICULTY
+  };
+
+  if (JSON.parse(localStorage.getItem("scores"))) {
+    var oldScores = JSON.parse(localStorage.getItem("scores"));
+    var newScores = oldScores;
+    newScores.push(score);
+    localStorage.setItem("scores", JSON.stringify(newScores));
+  } else {
+    var temp = [];
+    temp.push(score);
+    localStorage.setItem("scores", JSON.stringify(temp));
+  }
   if (!minutes) {
     alert("Vous avez gagné en " + seconds + " secondes ! ");
   } else {
@@ -80,13 +102,39 @@ var win = function() {
   location.reload();
 };
 
+var initialRender = function() {
+  var scores = JSON.parse(localStorage.getItem("scores"));
+
+  if (scores) {
+    highlights.classList.remove("hidden");
+    var ul = document.querySelector("ul");
+    scores.forEach(function(score) {
+      var li = document.createElement("li");
+
+      var text = "";
+
+      if (score.minutes) {
+        text += score.minutes + " minutes ";
+      }
+
+      text += score.seconds + " secondes ";
+
+      text += " - " + score.difficulty;
+
+      var textNode = document.createTextNode(text);
+      li.appendChild(textNode);
+      ul.appendChild(li);
+    });
+  }
+};
+
 var render = function() {
   var state = store.getState();
 
   points.innerHTML = state.points;
 
   if (state.points === 10) {
-    win();
+    win(state.difficulty);
   }
 
   holes[state.hole].classList.add("active");
@@ -107,9 +155,11 @@ var render = function() {
 
 var changeSpeed = function() {
   var newSpeed = store.getState().speed;
+  var newDifficulty = store.getState().difficulty;
 
   if (newSpeed !== localStorage.getItem("speed")) {
     localStorage.setItem("speed", newSpeed);
+    localStorage.setItem("difficulty", newDifficulty);
     location.reload();
   }
 };
@@ -145,17 +195,18 @@ for (i = 0; i < holes.length; i++) {
 }
 
 for (i = 0; i < buttons.length; i++) {
-  if (buttons[i].value === SPEED) {
+  if (parseInt(buttons[i].value) === parseInt(SPEED)) {
     buttons[i].style = "border: 3px solid red; border-radius: 5px";
   }
   buttons[i].addEventListener("click", function(e) {
-    console.log(e);
-    store.dispatch({type: "SET_SPEED", speed: parseInt(e.target.value)});
+    store.dispatch({type: "SET_SPEED", speed: parseInt(e.target.value), difficulty: e.target.innerHTML});
   });
 }
 
 launch.addEventListener("click", function() {
   store.dispatch({type: "START_GAME"});
 });
+
+initialRender();
 
 })();
